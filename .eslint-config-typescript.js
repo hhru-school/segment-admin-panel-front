18c16d319fd55a78c2c5ec8c22926c212ccd958a
vagrmv <59097564+vagrmv@users.noreply.github.com>
2023-04-07 18:54:22 +0300

    Initial commit
    * installed all packages. TODO: configure eslint
    
    * added some packages. configured eslint
    
    * Исправить замечания
    
    ---------
    
    Co-authored-by: Beken Sarsenbaev <sbeken6@gmail.com>

---
+++ .eslint-config-typescript.js
@@ -0,0 +1,51 @@
+module.exports = {
+    plugins: ['@typescript-eslint', 'etc'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        'react/prop-types': 'off',
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': [
+            'error',
+            {
+                functions: false,
+                classes: false,
+            },
+        ],
+        'no-void': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { vars: 'local', args: 'after-used', ignoreRestSiblings: true }],
+        'etc/prefer-interface': ['error', { allowLocal: true }],
+        'etc/no-misused-generics': 'error',
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: ['typeLike', 'enumMember'],
+                format: ['PascalCase'],
+            },
+        ],
+        '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+                checksVoidReturn: false,
+            },
+        ],
+    },
+    // Добавил настройки для eslint-import-resolver-typescript
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+};
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Segment admin panel (frontend)
+
+Этот проект был создан с помощью [Create React App](https://github.com/facebook/create-react-app).
+
+## Скрипты
+
+`yarn start` - запускает проект в режиме разработки.
+
+`yarn build` - запускает сборку проекта в папку `/build`.
+
+`yarn lint` - запускает проверку кода линтером.
