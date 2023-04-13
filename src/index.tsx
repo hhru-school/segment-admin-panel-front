ff633ad8deb6e31884501e6f8a2b7866204c4b6b
Beken Sarsenbaev <sbeken6@gmail.com>
2023-04-14 01:14:31 +0300

    Добавить тему

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 
 import { store } from 'store';
+import theme from 'theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <CssBaseline />
         <Provider store={store}>
+            <ThemeProvider theme={theme}>
+            </ThemeProvider>
         </Provider>
     </React.StrictMode>
 );
