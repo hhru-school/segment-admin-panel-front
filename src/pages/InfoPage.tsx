45f3851f66a90729ea406b88bddb5faa75d2b641
Beken Sarsenbaev <sbeken6@gmail.com>
2023-04-20 22:57:21 +0300

    Сверстать InfoPage

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,7 +1,80 @@
-import UnderConstructionPage from 'pages/UnderConstructionPage';
+import { shallowEqual } from 'react-redux';
+import Skeleton from '@mui/material/Skeleton';
+import Typography from '@mui/material/Typography';
+
+import DataCard from 'components/DataCard';
+import DataCardContent from 'components/DataCard/DataCardContent';
+import DataCardSubtitle from 'components/DataCard/DataCardSubtitle';
+import LayerStatusChip from 'components/LayerStatusChip';
+import ParentsLayersTable from 'components/ParentsLayersTable';
+import { useAppSelector } from 'hooks/redux-hooks';
+import { selectCurrentLayer, selectCurrentLayerLoadingStatus } from 'models/currentLayer';
 
 const InfoPage: React.FC = () => {
-    return <UnderConstructionPage pageName="Основная информация" />;
+    const layer = useAppSelector(selectCurrentLayer, shallowEqual);
+    const isLoading = useAppSelector(selectCurrentLayerLoadingStatus);
+
+    if (isLoading) {
+        return (
+            <>
+                <DataCard title="Общая информация" margin>
+                    <DataCardContent>
+                        <Skeleton variant="rounded" height={80} />
+                    </DataCardContent>
+                </DataCard>
+                <DataCard title="Родительский слой">
+                    <DataCardContent>
+                        <Skeleton variant="rounded" height={80} />
+                    </DataCardContent>
+                </DataCard>
+            </>
+        );
+    }
+
+    if (layer == null) {
+        return (
+            <>
+                <DataCard title="Общая информация" margin>
+                    <DataCardContent>
+                        <Typography>Нет данных.</Typography>
+                    </DataCardContent>
+                </DataCard>
+                <DataCard title="Родительский слой">
+                    <DataCardContent>
+                        <Typography>Нет данных.</Typography>
+                    </DataCardContent>
+                </DataCard>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <DataCard title="Общая информация" margin>
+                <DataCardContent>
+                    <LayerStatusChip status={layer.layerStatus} />
+                </DataCardContent>
+                <DataCardSubtitle>Описание</DataCardSubtitle>
+                <DataCardContent>
+                    <Typography>{layer.description}</Typography>
+                </DataCardContent>
+                <DataCardSubtitle>Дата создания</DataCardSubtitle>
+                <DataCardContent>
+                    <Typography>
+                        {new Date(layer.createTime).toLocaleString('ru', {
+                            dateStyle: 'long',
+                            timeStyle: 'medium',
+                        })}
+                    </Typography>
+                </DataCardContent>
+            </DataCard>
+            <DataCard title="Родительский слой">
+                <DataCardContent>
+                    <ParentsLayersTable />
+                </DataCardContent>
+            </DataCard>
+        </>
+    );
 };
 
 export default InfoPage;
