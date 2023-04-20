9184892b0c54fc330d23973daa03cd76f468e70b
Beken Sarsenbaev <sbeken6@gmail.com>
2023-04-20 22:56:47 +0300

    Добавить логику загузки данных в LayerPage

--- src/pages/LayerPage.tsx
+++ src/pages/LayerPage.tsx
@@ -1,4 +1,6 @@
-import { Outlet, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { shallowEqual } from 'react-redux';
+import { Outlet, redirect, useParams } from 'react-router-dom';
 import SegmentsIcon from '@mui/icons-material/DonutSmallOutlined';
 import InfoIcon from '@mui/icons-material/InfoOutlined';
 import LayerIcon from '@mui/icons-material/LibraryAddOutlined';
@@ -6,10 +8,41 @@ import EntryPointIcon from '@mui/icons-material/OpenInNewOutlined';
 import FieldsIcon from '@mui/icons-material/QuestionAnswerOutlined';
 import GroupFieldsIcon from '@mui/icons-material/QuizOutlined';
 
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
+import useErrorAlert from 'hooks/useErrorAlert';
 import LayerLayout from 'layouts/LayerLayout';
+import {
+    remove,
+    fetchLayer,
+    selectCurrentLayerTitle,
+    selectCurrentLayerError,
+    selectCurrentLayerLoadingStatus,
+} from 'models/currentLayer';
 
 const LayerPage: React.FC = () => {
-    const { entryPointId, fieldId } = useParams();
+    const { layerId, entryPointId, fieldId } = useParams();
+    const dispatch = useAppDispatch();
+    const title = useAppSelector(selectCurrentLayerTitle);
+    const error = useAppSelector(selectCurrentLayerError, shallowEqual);
+    const isLoading = useAppSelector(selectCurrentLayerLoadingStatus);
+    const { setAlert } = useErrorAlert();
+
+    useEffect(() => {
+        if (layerId != null) {
+            void dispatch(fetchLayer(Number(layerId)));
+        }
+        return () => void dispatch(remove());
+    }, [layerId, dispatch]);
+
+    useEffect(() => {
+        if (error != null) {
+            if (error.code === 404) {
+                redirect('/not-found');
+            } else {
+                setAlert(error.message);
+            }
+        }
+    }, [error, setAlert]);
 
     if (entryPointId != null || fieldId != null) {
         return <Outlet />;
@@ -25,7 +58,8 @@ const LayerPage: React.FC = () => {
                 { href: 'fields', primaryText: 'Поля', icon: <FieldsIcon /> },
                 { href: 'field-groups', primaryText: 'Группы полей', icon: <GroupFieldsIcon /> },
             ]}
-            title="Водитель за 50"
+            title={title}
+            loading={isLoading}
         >
             <Outlet />
         </LayerLayout>
