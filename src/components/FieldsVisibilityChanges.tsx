76ea0d8f3308d5c7988d99a0f8ddfd5e60612133
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-01 11:24:23 +0300

    Добавить компонент FieldsVisibilityChanges

---
+++ src/components/FieldsVisibilityChanges.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { shallowEqual } from 'react-redux';
+import Alert from '@mui/material/Alert';
+
+import { useAppSelector } from 'hooks/redux-hooks';
+import { selectQuestionActivatorLinkMap, selectLayerChangesLoadingStatus } from 'models/layerChanges';
+
+import ContentBox from 'components/ContentBox';
+import QuestionTable from 'components/QuestionTable';
+import Subtitle from 'components/Subtitle';
+
+const FieldsVisibilityChanges: React.FC = () => {
+    const isLoading = useAppSelector(selectLayerChangesLoadingStatus);
+    const links = useAppSelector(selectQuestionActivatorLinkMap, shallowEqual);
+
+    if (!isLoading && links === null) {
+        return (
+            <>
+                <Subtitle>Значения полей сегментов</Subtitle>
+                <ContentBox>
+                    <Alert severity="info">Изменений нет.</Alert>
+                </ContentBox>
+            </>
+        );
+    }
+
+    if (isLoading) {
+        return (
+            <>
+                <Subtitle>Значения полей сегментов</Subtitle>
+                <Subtitle variant="h6" loading={isLoading} SkeletonProps={{ sx: { width: '100%' } }} />
+                <ContentBox loading={isLoading} SkeletonProps={{ sx: { width: '100%', height: 200 } }} />
+            </>
+        );
+    }
+
+    return (
+        <>
+            <Subtitle>Значения полей сегментов</Subtitle>
+            {links !== null &&
+                Object.keys(links).map((segmentTitle) => {
+                    const data = links[segmentTitle];
+                    return (
+                        <React.Fragment key={segmentTitle}>
+                            <Subtitle variant="h6">{segmentTitle}</Subtitle>
+                            <ContentBox>
+                                {data === undefined ? (
+                                    <Alert severity="info">Изменений нет.</Alert>
+                                ) : (
+                                    <QuestionTable items={data} />
+                                )}
+                            </ContentBox>
+                        </React.Fragment>
+                    );
+                })}
+        </>
+    );
+};
+
+export default FieldsVisibilityChanges;
