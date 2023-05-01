7bcce813cd1aa85f7b432b735a79c81a7cf6b3c4
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-01 11:25:03 +0300

    Сверстать страницу LayerChangesPage

--- src/pages/LayerChangesPage.tsx
+++ src/pages/LayerChangesPage.tsx
@@ -1,7 +1,44 @@
-import UnderConstructionPage from 'pages/UnderConstructionPage';
+import { useEffect } from 'react';
+import { shallowEqual } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import EntryPointsChanges from 'components/EntryPointsChanges';
+import FieldsChanges from 'components/FieldsChanges';
+import FieldsVisibilityChanges from 'components/FieldsVisibilityChanges';
+import SegmentsChanges from 'components/SegmentsChanges';
+import Title from 'components/Title';
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
+import useErrorAlert from 'hooks/useErrorAlert';
+import { fetchLayerChanges, selectLayerChangesError, reset } from 'models/layerChanges';
 
 const LayerChangesPage: React.FC = () => {
-    return <UnderConstructionPage pageName="Изменения в слое" />;
+    const dispatch = useAppDispatch();
+    const { layerId } = useParams();
+    const { setAlert } = useErrorAlert();
+    const error = useAppSelector(selectLayerChangesError, shallowEqual);
+
+    useEffect(() => {
+        void dispatch(fetchLayerChanges(Number(layerId)));
+        return () => {
+            dispatch(reset());
+        };
+    }, [layerId, dispatch]);
+
+    useEffect(() => {
+        if (error !== null && error.code !== 404) {
+            setAlert(error.message);
+        }
+    }, [error, setAlert]);
+
+    return (
+        <>
+            <Title>Изменения в слое</Title>
+            <SegmentsChanges />
+            <EntryPointsChanges />
+            <FieldsChanges />
+            <FieldsVisibilityChanges />
+        </>
+    );
 };
 
 export default LayerChangesPage;
