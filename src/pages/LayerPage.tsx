40f75f885220ea0ae7513de217ac1280b1d0612c
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-02 13:22:21 +0300

    Удалить лишний эффект в LayerPage

--- src/pages/LayerPage.tsx
+++ src/pages/LayerPage.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { shallowEqual } from 'react-redux';
 import { Outlet, useParams, useNavigate } from 'react-router-dom';
 import SegmentsIcon from '@mui/icons-material/DonutSmallOutlined';
 import InfoIcon from '@mui/icons-material/InfoOutlined';
@@ -12,20 +11,13 @@ import { isApiError } from 'api';
 import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
 import useErrorAlert from 'hooks/useErrorAlert';
 import LayerLayout from 'layouts/LayerLayout';
-import {
-    fetchLayer,
-    selectCurrentLayerTitle,
-    selectCurrentLayerError,
-    selectCurrentLayerLoadingStatus,
-    reset,
-} from 'models/currentLayer';
+import { fetchLayer, selectCurrentLayerTitle, selectCurrentLayerLoadingStatus, reset } from 'models/currentLayer';
 
 const LayerPage: React.FC = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
     const { layerId, entryPointId, fieldId } = useParams();
     const isLoading = useAppSelector(selectCurrentLayerLoadingStatus);
-    const error = useAppSelector(selectCurrentLayerError, shallowEqual);
     const title = useAppSelector(selectCurrentLayerTitle);
     const { setAlert } = useErrorAlert();
 
@@ -33,20 +25,18 @@ const LayerPage: React.FC = () => {
         void dispatch(fetchLayer(Number(layerId)))
             .unwrap()
             .catch((error) => {
-                if (isApiError(error) && error.code === 404) {
-                    navigate('/not-found', { replace: true });
+                if (isApiError(error)) {
+                    if (error.code === 404) {
+                        navigate('/not-found', { replace: true });
+                    } else {
+                        setAlert(error.message);
+                    }
                 }
             });
         return () => {
             dispatch(reset());
         };
-    }, [layerId, dispatch, navigate]);
-
-    useEffect(() => {
-        if (error !== null && error.code !== 404) {
-            setAlert(error.message);
-        }
-    }, [error, setAlert]);
+    }, [layerId, dispatch, navigate, setAlert]);
 
     if (entryPointId !== undefined || fieldId !== undefined) {
         return <Outlet />;
