2f39bddab9610cd61c9ee6c43c8eef2985517e79
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-03 19:03:57 +0300

    Добавить заготовку ScreensPage

---
+++ src/pages/ScreensPage.tsx
@@ -0,0 +1,7 @@
+import UnderConstructionPage from 'pages/UnderConstructionPage';
+
+const ScreensPage: React.FC = () => {
+    return <UnderConstructionPage pageName="Экраны" />;
+};
+
+export default ScreensPage;
