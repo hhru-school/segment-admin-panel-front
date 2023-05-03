a63b4cb5d88a26c3bc987d3f1e0b270a9010ac09
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-03 18:55:31 +0300

    Изменить padding у Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,9 @@
 import { styled } from '@mui/material/styles';
 
 const Main = styled('main')(({ theme }) => ({
-    padding: theme.spacing(2),
+    padding: `0 ${theme.spacing(3)} ${theme.spacing(3)}`,
     [theme.breakpoints.up('sm')]: {
-        padding: theme.spacing(3),
+        padding: `0 ${theme.spacing(6)} ${theme.spacing(6)}`,
     },
     flexGrow: 1,
 }));
