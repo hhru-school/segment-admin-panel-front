6f11c305423a8f8a0c52e101ac5d18a502d5a78d
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-12 01:51:28 +0300

    Сверстать страницу FieldPage

--- src/pages/FieldPage.tsx
+++ src/pages/FieldPage.tsx
@@ -1,11 +1,48 @@
-import DetailsLayout from 'layouts/DetailsLayout';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import UnderConstructionPage from 'pages/UnderConstructionPage';
+import { isApiError } from 'api';
+import ContentBox from 'components/ContentBox';
+import FieldTree from 'components/FieldTree';
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
+import useErrorAlert from 'hooks/useErrorAlert';
+import DetailsLayout from 'layouts/DetailsLayout';
+import { fetchField, reset, selectCurrentFieldTitle, selectCurrentFieldLoadingStatus } from 'models/currentField';
 
 const FieldPage: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { setAlert } = useErrorAlert();
+    const navigate = useNavigate();
+    const { fieldId } = useParams();
+    const isLoading = useAppSelector(selectCurrentFieldLoadingStatus);
+    const title = useAppSelector(selectCurrentFieldTitle);
+
+    useEffect(() => {
+        void dispatch(fetchField({ layerId: 7, questionId: Number(fieldId) }))
+            .unwrap()
+            .catch((error) => {
+                if (isApiError(error)) {
+                    if (error.code === 404) {
+                        navigate('/not-found', { replace: true });
+                    } else {
+                        setAlert(error.message);
+                    }
+                }
+            });
+        return () => {
+            dispatch(reset());
+        };
+    }, [fieldId, dispatch, navigate, setAlert]);
+
     return (
-        <DetailsLayout title="Поле такое-то">
-            <UnderConstructionPage pageName="Поле" />
+        <DetailsLayout title={title} loading={isLoading} ContainerProps={{ maxWidth: 'md' }}>
+            <ContentBox
+                loading={isLoading}
+                SkeletonProps={{ sx: { height: '128px', width: '100%', maxWidth: 'none' } }}
+                sx={{ pt: 4 }}
+            >
+                <FieldTree />
+            </ContentBox>
         </DetailsLayout>
     );
 };
