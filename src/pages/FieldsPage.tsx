a27e396fa295c483eb9d576bf481dde2e050cf8b
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-18 17:32:43 +0300

    Исправить FieldsPage

--- src/pages/FieldsPage.tsx
+++ src/pages/FieldsPage.tsx
@@ -1,13 +1,13 @@
 import { useCallback, useEffect } from 'react';
 import { shallowEqual } from 'react-redux';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 import AddButton from 'components/AddButton';
-import AddButtonWrapper from 'components/AddButton/AddButtonWrapper';
 import ContentBox from 'components/ContentBox';
 import FieldsTree from 'components/FieldsTree';
 import SearchForm from 'components/SearchForm';
-import Title from 'components/Title';
 import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
 import useErrorAlert from 'hooks/useErrorAlert';
 import { fetchFields, reset, selectFieldsLoadingStatus, selectFieldsError, setSearchString } from 'models/fields';
@@ -41,15 +41,23 @@ const FieldsPage: React.FC = () => {
 
     return (
         <>
-            <Title>Поля</Title>
-            <AddButtonWrapper>
-                <AddButton href="/new/field" disabled={isLoading}>
-                    Добавить поле
-                </AddButton>
-            </AddButtonWrapper>
-            <Box sx={{ mb: 2 }}>
-                <SearchForm onSubmit={handleSearch} disabled={isLoading} />
-            </Box>
+            <Stack direction="row" alignItems="center" gap={4} sx={{ pt: 5, pb: 4 }}>
+                <Box sx={{ flexBasis: 140 }}>
+                    <Typography component="h2" variant="h5">
+                        Поля
+                    </Typography>
+                </Box>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Box sx={{ maxWidth: 700, minWidth: 340 }}>
+                        <SearchForm onSubmit={handleSearch} disabled={isLoading} />
+                    </Box>
+                </Box>
+                <Box sx={{ flexShrink: 0 }}>
+                    <AddButton href="/new/field" disabled={isLoading}>
+                        Добавить поле
+                    </AddButton>
+                </Box>
+            </Stack>
             <ContentBox loading={isLoading} SkeletonProps={{ height: 48, width: '100%' }}>
                 <FieldsTree />
             </ContentBox>
