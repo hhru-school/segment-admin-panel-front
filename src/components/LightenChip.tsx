634be93b7061736d6f2a8613b7d5a367d5ae7dd0
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-25 01:46:46 +0300

    Исправить цвет кнопки удаления у LightenChip

--- src/components/LightenChip.tsx
+++ src/components/LightenChip.tsx
@@ -3,33 +3,69 @@ import { styled, lighten } from '@mui/material/styles';
 
 export type LightenChipProps = Omit<ChipProps, 'variant'>;
 
-const LightenChip = styled(Chip, { shouldForwardProp: (prop) => prop !== 'color' })<LightenChipProps>(
-    ({ theme, color }) => ({
-        ...(color === 'primary' && {
+const LightenChip = styled(Chip, {
+    shouldForwardProp: (prop) => prop !== 'color',
+})<LightenChipProps>(({ theme, color }) => ({
+    ...(color === 'primary' && {
+        color: theme.palette.primary.dark,
+        backgroundColor: lighten(theme.palette.primary.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.primary.dark,
-            backgroundColor: lighten(theme.palette.primary.main, 0.9),
-        }),
-        ...(color === 'secondary' && {
+            '&:hover': {
+                color: theme.palette.primary.main,
+            },
+        },
+    }),
+    ...(color === 'secondary' && {
+        color: theme.palette.secondary.dark,
+        backgroundColor: lighten(theme.palette.secondary.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.secondary.dark,
-            backgroundColor: lighten(theme.palette.secondary.main, 0.9),
-        }),
-        ...(color === 'info' && {
+            '&:hover': {
+                color: theme.palette.secondary.main,
+            },
+        },
+    }),
+    ...(color === 'info' && {
+        color: theme.palette.info.dark,
+        backgroundColor: lighten(theme.palette.info.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.info.dark,
-            backgroundColor: lighten(theme.palette.info.main, 0.9),
-        }),
-        ...(color === 'success' && {
+            '&:hover': {
+                color: theme.palette.info.main,
+            },
+        },
+    }),
+    ...(color === 'success' && {
+        color: theme.palette.success.dark,
+        backgroundColor: lighten(theme.palette.success.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.success.dark,
-            backgroundColor: lighten(theme.palette.success.main, 0.9),
-        }),
-        ...(color === 'error' && {
+            '&:hover': {
+                color: theme.palette.success.main,
+            },
+        },
+    }),
+    ...(color === 'error' && {
+        color: theme.palette.error.dark,
+        backgroundColor: lighten(theme.palette.error.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.error.dark,
-            backgroundColor: lighten(theme.palette.error.main, 0.9),
-        }),
-        ...(color === 'warning' && {
+            '&:hover': {
+                color: theme.palette.error.main,
+            },
+        },
+    }),
+    ...(color === 'warning' && {
+        color: theme.palette.warning.dark,
+        backgroundColor: lighten(theme.palette.warning.main, 0.9),
+        '& .MuiChip-deleteIcon': {
             color: theme.palette.warning.dark,
-            backgroundColor: lighten(theme.palette.warning.main, 0.9),
-        }),
-    })
-);
+            '&:hover': {
+                color: theme.palette.warning.main,
+            },
+        },
+    }),
+}));
 
 export default LightenChip;
