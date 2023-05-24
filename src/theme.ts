d26532e1a0dd133e103d65aa0d0e43ed6382416f
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-25 01:45:29 +0300

    Добавить локализацию в тему

--- src/theme.ts
+++ src/theme.ts
@@ -1,23 +1,27 @@
 import { LinkProps } from '@mui/material/Link';
+import { ruRU } from '@mui/material/locale';
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 import LinkBehavior from 'components/LinkBehavior';
 
-const theme = createTheme({
-    // https://mui.com/material-ui/guides/routing/#global-theme-link
-    components: {
-        MuiLink: {
-            defaultProps: {
-                component: LinkBehavior,
-            } as LinkProps,
-        },
-        MuiButtonBase: {
-            defaultProps: {
-                LinkComponent: LinkBehavior,
+const theme = createTheme(
+    {
+        // https://mui.com/material-ui/guides/routing/#global-theme-link
+        components: {
+            MuiLink: {
+                defaultProps: {
+                    component: LinkBehavior,
+                } as LinkProps,
+            },
+            MuiButtonBase: {
+                defaultProps: {
+                    LinkComponent: LinkBehavior,
+                },
             },
         },
     },
-});
+    ruRU
+);
 
 const responsiveTypographyTheme = responsiveFontSizes(theme);
 
