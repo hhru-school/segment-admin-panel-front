d0b7fabd5ca88a2a6adf11233a9683d5a67793d7
Beken Sarsenbaev <sbeken6@gmail.com>
2023-05-29 21:42:34 +0300

    Исправить хелпер extractFinalFormErrorState

--- src/helpers/extractFinalFormErrorState.ts
+++ src/helpers/extractFinalFormErrorState.ts
@@ -2,10 +2,11 @@ import { FieldMetaState } from 'react-final-form';
 
 import isMessage from 'helpers/isMessage';
 
-const extractFinalFormErrorState = <T>(meta: FieldMetaState<T>): [boolean, string | null] => {
-    const isError = Boolean(isMessage(meta.error) && meta.touched);
+const extractFinalFormErrorState = <T>(meta: FieldMetaState<T>): [boolean, string | null, string | null] => {
     const error = isMessage(meta.error) ? meta.error : null;
-    return [isError, error];
+    const submitError = isMessage(meta.submitError) ? meta.submitError : null;
+    const isError = Boolean((error || submitError) && meta.touched && !meta.dirtySinceLastSubmit);
+    return [isError, error, submitError];
 };
 
 export default extractFinalFormErrorState;
