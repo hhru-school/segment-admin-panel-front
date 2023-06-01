014f79812c98f6c6a0aaeda22f2fcfebccb85bed
Beken Sarsenbaev <sbeken6@gmail.com>
2023-06-01 13:53:25 +0300

    Добавить пропс disabled в SearchView

--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -3,16 +3,17 @@ import Typography, { TypographyProps } from '@mui/material/Typography';
 interface SearchViewProps extends Omit<TypographyProps, 'children'> {
     children: string;
     subString: string;
+    disabled?: boolean;
 }
 
-const SearchView: React.FC<SearchViewProps> = ({ children, subString, ...rest }) => {
+const SearchView: React.FC<SearchViewProps> = ({ children, subString, disabled, ...rest }) => {
     const firstIndex = children.search(new RegExp(subString, 'i'));
     const lastIndex = firstIndex + subString?.length;
 
     return (
-        <Typography component="span" {...rest}>
+        <Typography component="span" sx={{ color: disabled ? 'text.disabled' : 'inherit' }} {...rest}>
             {children.slice(0, firstIndex)}
-            <Typography component="span" sx={{ fontWeight: 600, color: 'info.main' }}>
+            <Typography component="span" sx={{ fontWeight: 600, color: disabled ? 'text.disabled' : 'info.main' }}>
                 {children.slice(firstIndex, lastIndex)}
             </Typography>
             {children.slice(lastIndex)}
