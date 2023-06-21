c68a5f1e3180378e2ac9c6cad066eda82075e453
Beken Sarsenbaev <sbeken6@gmail.com>
2023-06-21 18:15:34 +0300

    Исправить AddingLayerForm

--- src/components/AddingLayerForm/index.tsx
+++ src/components/AddingLayerForm/index.tsx
@@ -2,41 +2,67 @@ import { useEffect } from 'react';
 import { Form } from 'react-final-form';
 import { shallowEqual } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { FORM_ERROR } from 'final-form';
 
+import api, { apiErrorHandler } from 'api';
 import Wizard, { Page } from 'components/Wizard';
 import isEmpty from 'helpers/isEmpty';
-import sleep from 'helpers/sleep';
 import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
 import useErrorAlert from 'hooks/useErrorAlert';
 import FormLayout from 'layouts/FormLayout';
 import { fetchLayersList, reset, selectLayersListError } from 'models/layersList';
 import { FormError } from 'types/common';
-import { LayerStates, LayersListItem } from 'types/layer';
-import { SegmentsFieldValue } from 'types/segment';
+import { LayerStates } from 'types/layer';
 
+import getRequestBody from 'components/AddingLayerForm/helpers/getRequestBody';
+import addFieldToScreen from 'components/AddingLayerForm/mutators/addFieldToScreen';
+import calcNewScreenFieldsPosition from 'components/AddingLayerForm/mutators/calcNewScreenFieldsPosition';
+import calcNewScreensPosition from 'components/AddingLayerForm/mutators/calcNewScreensPosition';
+import initSegmentDetails from 'components/AddingLayerForm/mutators/initSegmentDetails';
+import initSegments from 'components/AddingLayerForm/mutators/initSegments';
+import removeFieldFromScreen from 'components/AddingLayerForm/mutators/removeFieldFromScreen';
+import removeScreen from 'components/AddingLayerForm/mutators/removeScreen';
+import resetSegment from 'components/AddingLayerForm/mutators/resetSegment';
+import resetSegments from 'components/AddingLayerForm/mutators/resetSegments';
+import updateSegmentFields from 'components/AddingLayerForm/mutators/updateSegmentFields';
 import LayerInfoPage from 'components/AddingLayerForm/pages/InfoPage';
+import ScreensPage from 'components/AddingLayerForm/pages/ScreensPage';
+import SegmentDetailsPage from 'components/AddingLayerForm/pages/SegmentDetailsPage';
 import LayerSegmentsPage from 'components/AddingLayerForm/pages/SegmentsPage';
+import { NewLayer, isPagesState } from 'components/AddingLayerForm/types';
 
 export const enum PageName {
     Info = 'Info',
     Segments = 'Segments',
-}
-export const enum FieldName {
-    Title = 'title',
-    Description = 'description',
-    ParentLayer = 'parentLayer',
-    Segments = 'segments',
-}
-interface Values {
-    [FieldName.Title]: string;
-    [FieldName.Description]: string;
-    [FieldName.ParentLayer]: LayersListItem | null;
-    [FieldName.Segments]: SegmentsFieldValue | null;
+    Details = 'Details',
+    Screens = 'Screens',
 }
 
-const INITIAL_VALUES: Values = { title: '', description: '', parentLayer: null, segments: null };
-const validator = (values: Values): FormError<Values> => {
-    const errors: FormError<Values> = {};
+const pages = new Map<`${PageName}`, Page>([
+    [PageName.Info, { name: PageName.Info, element: <LayerInfoPage /> }],
+    [PageName.Segments, { name: PageName.Segments, element: <LayerSegmentsPage /> }],
+    [PageName.Details, { name: PageName.Details, element: <SegmentDetailsPage /> }],
+    [PageName.Screens, { name: PageName.Screens, element: <ScreensPage /> }],
+]);
+
+const getTitle = (page?: Page, state?: unknown): string => {
+    if (page?.name) {
+        const segmentTitle = isPagesState(state) && state.segment !== null ? state.segment.title : '';
+        const titles = {
+            [`${PageName.Info}`]: 'Новый слой / Основная информация',
+            [`${PageName.Segments}`]: 'Новый слой / Сегменты',
+            [`${PageName.Details}`]: `Новый слой / ${segmentTitle}`,
+            [`${PageName.Screens}`]: `Новый слой / ${segmentTitle} / Добавление экранов`,
+        };
+        return titles[page.name] || '';
+    }
+    return '';
+};
+
+const INITIAL_VALUES: NewLayer = { title: '', description: '', parentLayer: null, segments: null };
+
+const validator = (values: NewLayer): FormError<NewLayer> => {
+    const errors: FormError<NewLayer> = {};
     if (!values.title) {
         errors.title = 'Пожалуйста, введите наименование слоя.';
     }
@@ -46,10 +72,16 @@ const validator = (values: Values): FormError<Values> => {
     return errors;
 };
 
-const pages = new Map<`${PageName}`, Page>([
-    [PageName.Info, { title: 'Новый слой / Основная информация', element: <LayerInfoPage /> }],
-    [PageName.Segments, { title: 'Новый слой / Сегменты', element: <LayerSegmentsPage /> }],
-]);
+const submitErrorHandler = (error: unknown): string => {
+    const DEFAULT_BAD_REQUEST_MESSAGE = 'Не удалось добавить слой. Проверьте введенные данные и повторите попытку.';
+    const apiError = apiErrorHandler(error);
+
+    if (apiError?.code === 400) {
+        return apiError?.data?.message || DEFAULT_BAD_REQUEST_MESSAGE;
+    }
+
+    return apiError.message;
+};
 
 const AddingLayerForm: React.FC = () => {
     const dispatch = useAppDispatch();
@@ -70,19 +102,23 @@ const AddingLayerForm: React.FC = () => {
         }
     }, [error, setAlert]);
 
-    const handleSubmit = async (values: Values) => {
-        await sleep(2000);
-        // eslint-disable-next-line no-console
-        console.log(values);
-        navigate('/layers');
+    const handleSubmit = async (values: NewLayer) => {
+        try {
+            await api.post<NewLayer>('/layers/add', getRequestBody(values));
+        } catch (error) {
+            const message = submitErrorHandler(error);
+            setAlert(message);
+            return { [FORM_ERROR]: message };
+        }
+        return navigate('/layers');
     };
 
     return (
         <Wizard pages={pages} defaultPage={PageName.Info}>
-            {({ activePage }) => {
+            {({ activePage, state }) => {
                 return (
-                    <FormLayout title={activePage?.title} ContainerProps={{ maxWidth: 'md' }}>
-                        <Form<Values>
+                    <FormLayout title={getTitle(activePage, state)} ContainerProps={{ maxWidth: 'md' }}>
+                        <Form<NewLayer>
                             onSubmit={handleSubmit}
                             initialValues={INITIAL_VALUES}
                             validate={validator}
@@ -91,6 +127,18 @@ const AddingLayerForm: React.FC = () => {
                                 pristine: true,
                                 submitSucceeded: true,
                                 submitError: true,
+                            }}
+                            mutators={{
+                                initSegments,
+                                resetSegments,
+                                resetSegment,
+                                initSegmentDetails,
+                                addFieldToScreen,
+                                removeFieldFromScreen,
+                                removeScreen,
+                                updateSegmentFields,
+                                calcNewScreensPosition,
+                                calcNewScreenFieldsPosition,
                             }}
                             render={({ handleSubmit }) => (
                                 <form autoComplete="off" onSubmit={handleSubmit}>
