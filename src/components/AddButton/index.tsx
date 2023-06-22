b77259b1e63371ee937c8cf3e1bf20d22d0cbdf2
Beken Sarsenbaev <sbeken6@gmail.com>
2023-06-22 13:34:18 +0300

    Исправить AddButton

--- src/components/AddButton/index.tsx
+++ src/components/AddButton/index.tsx
@@ -1,9 +1,14 @@
 import AddIcon from '@mui/icons-material/Add';
 import Button, { ButtonProps } from '@mui/material/Button';
 
-const AddButton: React.FC<Pick<ButtonProps, 'href' | 'disabled' | 'children'>> = ({ children, href, disabled }) => {
+const AddButton: React.FC<Pick<ButtonProps, 'onClick' | 'href' | 'disabled' | 'children'>> = ({
+    children,
+    onClick,
+    href,
+    disabled,
+}) => {
     return (
-        <Button href={href} startIcon={<AddIcon />} variant="contained" disabled={disabled}>
+        <Button onClick={onClick} href={href} startIcon={<AddIcon />} variant="contained" disabled={disabled}>
             {children}
         </Button>
     );
